e1af84dc4506913607f02040ab4a8baa33ab0fa0
Sayan Nath <sayannath235@Sayans-MacBook-Pro.local>
2019-12-22 01:27:35 +0530

    Final

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-import 'package:floating_search_bar/floating_search_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:dsckiit_app/screen/splashScreen.dart';
 import 'package:dsckiit_app/page/openingPage.dart';
@@ -17,25 +16,6 @@ class MyApp extends StatelessWidget {
         primaryColor: Colors.blue,
       ),
       home: SplashScreen(),
-      // home: FloatingSearchBar.builder(
-      //           itemCount: 100,
-      //           itemBuilder: (BuildContext context, int index) {
-      //             return ListTile(
-      //               leading: Text(index.toString()),
-      //             );
-      //           },
-      //           trailing: CircleAvatar(
-      //             child: Text("RD"),
-      //           ),
-      //           drawer: Drawer(
-      //             child: Container(),
-      //           ),
-      //           onChanged: (String value) {},
-      //           onTap: () {},
-      //           decoration: InputDecoration.collapsed(
-      //             hintText: "Search...",
-      //           ),
-      //         ),
       routes: <String, WidgetBuilder>{
         "/SigninPage": (BuildContext context) => SigninPage(),
         "/SignupPage": (BuildContext context) => SignupPage(),
