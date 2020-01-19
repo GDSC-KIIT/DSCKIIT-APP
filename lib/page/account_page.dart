c5435513b9a632d0664f05c6787219046f54ab9b
amansahu278 <a.man.cubes24@gmail.com>
2020-01-20 02:20:07 +0530

    Updates were made to the opening screen buttons and the media and account pages were made

---
+++ lib/page/account_page.dart
@@ -0,0 +1,46 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/material.dart';
+
+class AccountPage extends StatelessWidget {
+  AccountPage({this.user});
+
+  FirebaseUser user;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Account"),
+        centerTitle: true,
+        backgroundColor: Colors.indigoAccent[200],
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.start,
+          children: <Widget>[
+            SizedBox(
+              height: 50,
+            ),
+            CircleAvatar(
+              backgroundColor: Colors.white,
+              minRadius: 30,
+              child: Image.network(
+                  "https://static.thenounproject.com/png/17241-200.png"),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text(
+              "User name",
+              style: TextStyle(fontWeight: FontWeight.w500, fontSize: 40),
+            ),
+            SizedBox(
+              height: 20,
+            ),
+            Text("User email", style: TextStyle(fontSize: 20))
+          ],
+        ),
+      ),
+    );
+  }
+}
