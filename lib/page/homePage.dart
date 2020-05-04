83974c3cad3ca52f7eabd9c68e42618a59108593
sayannath <sayannath235@gmail.com>
2020-05-04 18:13:46 +0530

    UI

--- lib/page/homePage.dart
+++ lib/page/homePage.dart
@@ -60,193 +60,206 @@ class _HomePageState extends State<HomePage> {
     return SafeArea(
       child: Scaffold(
         backgroundColor: Colors.white,
-        body: FloatingSearchBar.builder(
-          pinned: true,
-          itemCount: 1,
-          itemBuilder: (BuildContext context, int index) {
-            return Padding(
-              padding: const EdgeInsets.only(left: 8.0),
-              child: Column(
+        body: Padding(
+          padding: const EdgeInsets.only(top: 12.0),
+          child: FloatingSearchBar.builder(
+            pinned: true,
+            itemCount: 1,
+            itemBuilder: (BuildContext context, int index) {
+              return Padding(
+                padding: const EdgeInsets.only(left: 8.0),
+                child: Column(
+                  children: <Widget>[
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Ongoing",
+                            style: kHeadingStyle,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.grey[900],
+                            ),
+                            iconSize: 27,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      child: ListView.builder(
+                        physics: BouncingScrollPhysics(),
+                        shrinkWrap: true,
+                        scrollDirection: Axis.horizontal,
+                        itemCount: 3,
+                        itemBuilder: (context, int index) {
+                          return CustomCard(
+                            title: 'American Sign Language Recognition',
+                            members: index + 1,
+                            color: Colors.indigo,
+                          );
+                        },
+                      ),
+                    ),
+                    SizedBox(
+                      height: 20,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(8.0),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                        children: <Widget>[
+                          Text(
+                            "Events and Schedules",
+                            style: kHeadingStyle,
+                          ),
+                          IconButton(
+                            icon: Icon(
+                              Icons.arrow_forward,
+                              color: Colors.grey[900],
+                            ),
+                            iconSize: 27,
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                    ),
+                    Container(
+                      height: 150,
+                      child: StreamBuilder<QuerySnapshot>(
+                        stream:
+                            Firestore.instance.collection('events').snapshots(),
+                        builder: (BuildContext context,
+                            AsyncSnapshot<QuerySnapshot> snapshot) {
+                          if (snapshot.hasError)
+                            return new Text('Error: ${snapshot.error}');
+                          switch (snapshot.connectionState) {
+                            case ConnectionState.waiting:
+                              return Center(child: CircularProgressIndicator());
+                            default:
+                              return new ListView(
+                                physics: BouncingScrollPhysics(),
+                                shrinkWrap: true,
+                                scrollDirection: Axis.horizontal,
+                                children: snapshot.data.documents
+                                    .map((DocumentSnapshot document) {
+                                  return new CustomEventCard(
+                                    title: document['title'],
+                                    imgURL: document['image'],
+                                    date: document['date'],
+                                    registerUrl: document['register'],
+                                  );
+                                }).toList(),
+                              );
+                          }
+                        },
+                      ),
+                      // child: ListView.builder(
+                      //   physics: BouncingScrollPhysics(),
+                      //   shrinkWrap: true,
+                      //   scrollDirection: Axis.horizontal,
+                      //   itemCount: 10,
+                      //   itemBuilder: (context, int index) {
+                      //     return CustomEventCard(
+                      //       title: 'Sample title',
+                      //       imgURL:
+                      //           'https://www.invensis.net/blog/wp-content/uploads/2015/05/Benefits-of-Python-over-other-programming-languages-invensis.jpg',
+                      //       date: '31st October',
+                      //     );
+                      //   },
+                      // ),
+                    ),
+                  ],
+                ),
+              );
+            },
+            trailing: !isSignedIn
+                ? CircleAvatar(
+                    backgroundImage: AssetImage("assets/animator.gif"),
+                    backgroundColor: Colors.transparent,
+                  )
+                : CircleAvatar(
+                    backgroundImage: user!=null ? user.photoUrl!=null ? NetworkImage(user.photoUrl) : AssetImage('assets/user.png') : AssetImage('assets/user.png'),
+                    backgroundColor: Colors.transparent,
+                  ),
+            drawer: Drawer(
+              child: ListView(
                 children: <Widget>[
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Ongoing",
-                          style: kHeadingStyle,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.arrow_forward,
-                            color: Colors.grey[900],
+                  DrawerHeader(
+                    decoration: BoxDecoration(
+                        color: Colors.black,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/logo.png',
                           ),
-                          iconSize: 27,
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
+                          fit: BoxFit.contain,
+                        )),
+                    //child: Text('Header'),
                   ),
-                  Container(
-                    height: 150,
-                    child: ListView.builder(
-                      physics: BouncingScrollPhysics(),
-                      shrinkWrap: true,
-                      scrollDirection: Axis.horizontal,
-                      itemCount: 3,
-                      itemBuilder: (context, int index) {
-                        return CustomCard(
-                          title: 'American Sign Language Recognition',
-                          members: index + 1,
-                          color: Colors.indigo,
-                        );
-                      },
-                    ),
+                  ListTile(
+                    title: Text("Accounts"),
+                    trailing: Icon(Icons.person),
+                    onTap: () {
+                      Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => AccountPage(user: user)));
+                    },
                   ),
-                  SizedBox(
-                    height: 20,
+                  ListTile(
+                    title: Text("Mentors"),
+                    trailing: Icon(Icons.person),
+                    onTap: () {
+                      // Navigator.push(
+                      //     context,
+                      //     MaterialPageRoute(
+                      //         builder: (context) => AccountPage(user: user)));
+                    },
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: <Widget>[
-                        Text(
-                          "Events and Schedules",
-                          style: kHeadingStyle,
-                        ),
-                        IconButton(
-                          icon: Icon(
-                            Icons.arrow_forward,
-                            color: Colors.grey[900],
-                          ),
-                          iconSize: 27,
-                          onPressed: () {},
-                        ),
-                      ],
-                    ),
+                  ListTile(
+                    title: Text("Chat"),
+                    trailing: Icon(Icons.message),
+                    onTap: () {
+                      Navigator.of(context)
+                          .push(MaterialPageRoute(builder: (context) {
+                        return ChatContainer();
+                      }));
+                    },
                   ),
-                  Container(
-                    height: 150,
-                    child: StreamBuilder<QuerySnapshot>(
-                      stream:
-                          Firestore.instance.collection('events').snapshots(),
-                      builder: (BuildContext context,
-                          AsyncSnapshot<QuerySnapshot> snapshot) {
-                        if (snapshot.hasError)
-                          return new Text('Error: ${snapshot.error}');
-                        switch (snapshot.connectionState) {
-                          case ConnectionState.waiting:
-                            return Center(child: CircularProgressIndicator());
-                          default:
-                            return new ListView(
-                              physics: BouncingScrollPhysics(),
-                              shrinkWrap: true,
-                              scrollDirection: Axis.horizontal,
-                              children: snapshot.data.documents
-                                  .map((DocumentSnapshot document) {
-                                return new CustomEventCard(
-                                  title: document['title'],
-                                  imgURL: document['image'],
-                                  date: document['date'],
-                                  registerUrl: document['register'],
-                                );
-                              }).toList(),
-                            );
-                        }
-                      },
-                    ),
-                    // child: ListView.builder(
-                    //   physics: BouncingScrollPhysics(),
-                    //   shrinkWrap: true,
-                    //   scrollDirection: Axis.horizontal,
-                    //   itemCount: 10,
-                    //   itemBuilder: (context, int index) {
-                    //     return CustomEventCard(
-                    //       title: 'Sample title',
-                    //       imgURL:
-                    //           'https://www.invensis.net/blog/wp-content/uploads/2015/05/Benefits-of-Python-over-other-programming-languages-invensis.jpg',
-                    //       date: '31st October',
-                    //     );
-                    //   },
-                    // ),
+                  ListTile(
+                    title: Text("Noticeboard"),
+                    trailing: Icon(Icons.photo),
+                    onTap: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => MediaPage()));
+                    },
+                  ),
+                  ListTile(
+                    title: Text("Feedback From"),
+                    trailing: Icon(Icons.feedback),
+                    onTap: () {},
+                  ),
+                  Divider(),
+                  ListTile(
+                    title: Text("Close"),
+                    trailing: Icon(Icons.close),
+                    onTap: () {
+                      Navigator.of(context).pop();
+                    },
                   ),
                 ],
               ),
-            );
-          },
-          trailing: !isSignedIn
-              ? CircleAvatar(
-                  backgroundImage: AssetImage("assets/animator.gif"),
-                  backgroundColor: Colors.transparent,
-                )
-              : CircleAvatar(
-                  backgroundImage: user!=null ? user.photoUrl!=null ? NetworkImage(user.photoUrl) : AssetImage('assets/user.png') : AssetImage('assets/user.png'),
-                  backgroundColor: Colors.transparent,
-                ),
-          drawer: Drawer(
-            child: ListView(
-              children: <Widget>[
-                DrawerHeader(
-                  decoration: BoxDecoration(
-                      color: Colors.black,
-                      image: DecorationImage(
-                        image: AssetImage(
-                          'assets/logo.png',
-                        ),
-                        fit: BoxFit.contain,
-                      )),
-                  //child: Text('Header'),
-                ),
-                ListTile(
-                  title: Text("Accounts"),
-                  trailing: Icon(Icons.person),
-                  onTap: () {
-                    Navigator.push(
-                        context,
-                        MaterialPageRoute(
-                            builder: (context) => AccountPage(user: user)));
-                  },
-                ),
-                ListTile(
-                  title: Text("Chat"),
-                  trailing: Icon(Icons.message),
-                  onTap: () {
-                    Navigator.of(context)
-                        .push(MaterialPageRoute(builder: (context) {
-                      return ChatContainer();
-                    }));
-                  },
-                ),
-                ListTile(
-                  title: Text("Noticeboard"),
-                  trailing: Icon(Icons.photo),
-                  onTap: () {
-                    Navigator.push(context,
-                        MaterialPageRoute(builder: (context) => MediaPage()));
-                  },
-                ),
-                ListTile(
-                  title: Text("Feedback From"),
-                  trailing: Icon(Icons.feedback),
-                  onTap: () {},
-                ),
-                Divider(),
-                ListTile(
-                  title: Text("Close"),
-                  trailing: Icon(Icons.close),
-                  onTap: () {
-                    Navigator.of(context).pop();
-                  },
-                ),
-              ],
             ),
-          ),
-          onChanged: (String value) {},
-          onTap: () {},
-          decoration: InputDecoration.collapsed(
-            hintText: "Search events, people etc.",
+            onChanged: (String value) {},
+            onTap: () {},
+            decoration: InputDecoration.collapsed(
+              hintText: "Search events, people etc.",
+            ),
           ),
         ),
         floatingActionButton: FloatingActionButton(
