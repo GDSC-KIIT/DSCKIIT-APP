ea32ef81262e75e60b55ff98be854e1d943fcfd2
amansahu278 <54412402+amansahu278@users.noreply.github.com>
2020-05-09 18:06:33 +0530

    AdditionalInfo instead of feedbackform
    for testing

--- lib/page/homePage.dart
+++ lib/page/homePage.dart
@@ -502,7 +502,7 @@ class _HomePageState extends State<HomePage> {
                         Navigator.push(
                             context,
                             MaterialPageRoute(
-                                builder: (context) => FeedBackPage()));
+                                builder: (context) => AdditionalInfoScreen()));
                       },
                     ),
                     Divider(),
