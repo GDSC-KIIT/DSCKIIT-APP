815109d99b3abd935f745ea15968546ee46e400f
amansahu278 <a.man.cubes24@gmail.com>
2020-05-23 13:06:54 +0530

    Add Notes update
    1. Overflow fixed
    2. Trimmed

--- lib/notes/addNotes.dart
+++ lib/notes/addNotes.dart
@@ -33,69 +33,73 @@ class _NoteScreenState extends State<NoteScreen> {
         title: Text('Notes'),
         centerTitle: true,
       ),
-      body: Center(
-        child: Container(
-          margin: EdgeInsets.all(15.0),
-          alignment: Alignment.center,
-          child: Column(
-            children: <Widget>[
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: TextField(
-                  controller: _titleController,
-                  decoration: InputDecoration(
-                      labelText: 'Title',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0))),
+      body: SingleChildScrollView(
+        child : Center(
+          child: Container(
+            margin: EdgeInsets.all(15.0),
+            alignment: Alignment.center,
+            child: Column(
+              children: <Widget>[
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: TextField(
+                    controller: _titleController,
+                    decoration: InputDecoration(
+                        labelText: 'Title',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
                 ),
-              ),
-              Padding(padding: new EdgeInsets.all(5.0)),
-              Container(
-                margin: EdgeInsets.only(top: 20),
-                child: TextField(
-                  controller: _descriptionController,
-                  decoration: InputDecoration(
-                      labelText: 'Description',
-                      border: OutlineInputBorder(
-                          borderRadius: BorderRadius.circular(5.0))),
+                Padding(padding: new EdgeInsets.all(5.0)),
+                Container(
+                  margin: EdgeInsets.only(top: 20),
+                  child: TextField(
+                    minLines: 1,
+                    maxLines: 100,
+                    controller: _descriptionController,
+                    decoration: InputDecoration(
+                        labelText: 'Description',
+                        border: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(5.0))),
+                  ),
                 ),
-              ),
-              Padding(padding: new EdgeInsets.all(5.0)),
-              Container(
-                padding: EdgeInsets.only(top: 20.0),
-                child: RaisedButton(
-                  padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
-                  onPressed: () {
-                    if (widget.note.id != null) {
-                      db
-                          .updateNote(Note(
-                              widget.note.id,
-                              _titleController.text,
-                              _descriptionController.text))
-                          .then((_) {
-                        Navigator.pop(context);
-                      });
-                    } else {
-                      db
-                          .createNote(_titleController.text,
-                              _descriptionController.text)
-                          .then((_) {
-                        Navigator.pop(context);
-                      });
-                    }
-                  },
-                  color: Color(0xFF183E8D),
-                  child: (widget.note.id != null)
-                      ? Text('Update',
-                          style: TextStyle(fontSize: 20.0, color: Colors.white))
-                      : Text('Add',
-                          style:
-                              TextStyle(fontSize: 20.0, color: Colors.white)),
-                  shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(20.0)),
-                ),
-              )
-            ],
+                Padding(padding: new EdgeInsets.all(5.0)),
+                Container(
+                  padding: EdgeInsets.only(top: 20.0),
+                  child: RaisedButton(
+                    padding: EdgeInsets.fromLTRB(100, 20, 100, 20),
+                    onPressed: () {
+                      if (widget.note.id != null) {
+                        db
+                            .updateNote(Note(
+                                widget.note.id,
+                                _titleController.text.trim(),
+                                _descriptionController.text.trim()))
+                            .then((_) {
+                          Navigator.pop(context);
+                        });
+                      } else {
+                        db
+                            .createNote(_titleController.text.trim(),
+                                _descriptionController.text.trim())
+                            .then((_) {
+                          Navigator.pop(context);
+                        });
+                      }
+                    },
+                    color: Color(0xFF183E8D),
+                    child: (widget.note.id != null)
+                        ? Text('Update',
+                            style: TextStyle(fontSize: 20.0, color: Colors.white))
+                        : Text('Add',
+                            style:
+                                TextStyle(fontSize: 20.0, color: Colors.white)),
+                    shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(20.0)),
+                  ),
+                )
+              ],
+            ),
           ),
         ),
       ),
