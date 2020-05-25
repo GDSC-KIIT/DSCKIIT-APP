7ceaf189c5b0344e213f9101f8272ff74310c28c
Amrit Dash <amrit.dash60@gmail.com>
2020-05-26 03:32:09 +0530

    Added Toast

--- lib/page/markAttendance.dart
+++ lib/page/markAttendance.dart
@@ -1,7 +1,8 @@
 import 'dart:collection';
-
+import 'package:dsckiit_app/constants.dart';
 import 'package:flutter/material.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:toast/toast.dart';
 
 class MarkAttendance extends StatefulWidget {
   String uid;
@@ -11,14 +12,17 @@ class MarkAttendance extends StatefulWidget {
 }
 
 class _MarkAttendanceState extends State<MarkAttendance> {
-
   List<String> _attendees = new List();
 
-  void populateAttendees() async{
-    await Firestore.instance.collection('meetings').document(widget.uid).get().then((DocumentSnapshot documentSnapshot){
-      if(documentSnapshot.data['attendees'] != null){
+  void populateAttendees() async {
+    await Firestore.instance
+        .collection('meetings')
+        .document(widget.uid)
+        .get()
+        .then((DocumentSnapshot documentSnapshot) {
+      if (documentSnapshot.data['attendees'] != null) {
         print(documentSnapshot.data['attendees']);
-        documentSnapshot.data['attendees'].forEach((uid){
+        documentSnapshot.data['attendees'].forEach((uid) {
           _attendees.add(uid);
         });
       }
@@ -35,15 +39,15 @@ class _MarkAttendanceState extends State<MarkAttendance> {
     print(_attendees);
   }
 
-  bool onChoice(String uid){
-    if(_attendees.indexOf(uid) != -1){
-        setState(() {
-          _attendees.removeAt(_attendees.indexOf(uid));
-        });
-        return false;
-    }else{
+  bool onChoice(String uid) {
+    if (_attendees.indexOf(uid) != -1) {
       setState(() {
-        if(uid != null){
+        _attendees.removeAt(_attendees.indexOf(uid));
+      });
+      return false;
+    } else {
+      setState(() {
+        if (uid != null) {
           _attendees.add(uid);
         }
       });
@@ -56,39 +60,62 @@ class _MarkAttendanceState extends State<MarkAttendance> {
     return Scaffold(
       body: StreamBuilder(
         stream: Firestore.instance.collection('users').snapshots(),
-        builder: (context, snapshot){
-          if(!snapshot.hasData) return CircularProgressIndicator();
+        builder: (context, snapshot) {
+          if (!snapshot.hasData) return CircularProgressIndicator();
           return ListView(
-            children: snapshot.data.documents.map<Widget>((DocumentSnapshot documentSnapshot){
-              return documentSnapshot.data['uid'] == null ? SizedBox() : Padding(
-                padding: const EdgeInsets.only(left: 8.0, top: 10, right: 8.0),
-                child: AttendeeCard(
-                  ds: documentSnapshot,
-                  onChangeHandler: onChoice,
-                  value: _attendees.indexOf(documentSnapshot.data['uid']) == -1 ? false : true,
-                ),
-              );
+            children: snapshot.data.documents
+                .map<Widget>((DocumentSnapshot documentSnapshot) {
+              return documentSnapshot.data['uid'] == null
+                  ? SizedBox()
+                  : Padding(
+                      padding:
+                          const EdgeInsets.only(left: 8.0, top: 10, right: 8.0),
+                      child: AttendeeCard(
+                        ds: documentSnapshot,
+                        onChangeHandler: onChoice,
+                        value:
+                            _attendees.indexOf(documentSnapshot.data['uid']) ==
+                                    -1
+                                ? false
+                                : true,
+                      ),
+                    );
             }).toList(),
           );
         },
       ),
       floatingActionButton: FloatingActionButton.extended(
-          icon: Icon(Icons.save),
-          onPressed: () async{
-            List<String> _absentees = new List();
-            await Firestore.instance.collection('users').getDocuments().then((QuerySnapshot snapshot){
-              snapshot.documents.forEach((element) {
-                if(_attendees.indexOf(element.data['uid']) == -1){
-                  if(element.data['uid'] != null){
-                    _absentees.add(element.data['uid']);
-                  }
+        icon: Icon(Icons.save),
+        onPressed: () async {
+          List<String> _absentees = new List();
+          await Firestore.instance
+              .collection('users')
+              .getDocuments()
+              .then((QuerySnapshot snapshot) {
+            snapshot.documents.forEach((element) {
+              if (_attendees.indexOf(element.data['uid']) == -1) {
+                if (element.data['uid'] != null) {
+                  _absentees.add(element.data['uid']);
                 }
-              });
+              }
             });
-            _absentees = LinkedHashSet<String>.from(_absentees).toList();
-            Firestore.instance.collection('meetings').document(widget.uid).updateData({"attendees": _attendees, "absentees" : _absentees});
-          },
-          label: Text("Save"),
+          });
+          _absentees = LinkedHashSet<String>.from(_absentees).toList();
+          Firestore.instance
+              .collection('meetings')
+              .document(widget.uid)
+              .updateData({"attendees": _attendees, "absentees": _absentees});
+          Toast.show(
+            "Marked",
+            context,
+            duration: Toast.LENGTH_LONG,
+            gravity: Toast.BOTTOM,
+            backgroundColor: Colors.lightGreenAccent[100],
+            backgroundRadius: 18.0,
+            textColor: kFabColor,
+          );
+        },
+        label: Text("Save"),
       ),
     );
   }
@@ -105,12 +132,11 @@ class AttendeeCard extends StatefulWidget {
 }
 
 class _AttendeeCardState extends State<AttendeeCard> {
-
   @override
   Widget build(BuildContext context) {
     bool value = widget.value;
     return GestureDetector(
-      onTap: (){
+      onTap: () {
         widget.onChangeHandler(widget.ds.data['uid']);
       },
       child: Container(
@@ -127,23 +153,36 @@ class _AttendeeCardState extends State<AttendeeCard> {
                   height: 50,
                   width: 50,
                   placeholder: "assets/mascot.png",
-                  image: widget.ds.data['photoURL'].toString().isNotEmpty ? widget.ds.data['photoURL'] :
-                  'https://firebasestorage.googleapis.com/v0/b/myra-health.appspot.com/o/pf.png?alt=media&token=0a4f0eef-0aac-4b76-9cea-5f0f2bcde42f',
+                  image: widget.ds.data['photoURL'].toString().isNotEmpty
+                      ? widget.ds.data['photoURL']
+                      : 'https://firebasestorage.googleapis.com/v0/b/myra-health.appspot.com/o/pf.png?alt=media&token=0a4f0eef-0aac-4b76-9cea-5f0f2bcde42f',
                   fit: BoxFit.cover,
                 ),
               ),
-              SizedBox(width: 10,),
+              SizedBox(
+                width: 10,
+              ),
               Column(
                 mainAxisAlignment: MainAxisAlignment.center,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(widget.ds.data['displayName'], style: TextStyle(fontWeight: FontWeight.w500, fontSize: 23),),
+                    child: Text(
+                      widget.ds.data['displayName'],
+                      style:
+                          TextStyle(fontWeight: FontWeight.w500, fontSize: 23),
+                    ),
                   ),
                   Container(
                     padding: EdgeInsets.symmetric(horizontal: 10),
-                    child: Text(widget.ds.data['email'], style: TextStyle(color: Colors.blueGrey, fontWeight: FontWeight.w500, fontSize: 10),),
+                    child: Text(
+                      widget.ds.data['email'],
+                      style: TextStyle(
+                          color: Colors.blueGrey,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 10),
+                    ),
                   )
                 ],
               ),
