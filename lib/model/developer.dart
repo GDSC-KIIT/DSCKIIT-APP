ca7d3d59d6bb35a691cb4e8e66349d58f3e963ab
Amrit Dash <amrit.dash60@gmail.com>
2020-06-01 19:17:47 +0530

    Model for Developer Page

---
+++ lib/model/developer.dart
@@ -0,0 +1,30 @@
+class Developer {
+  String speakerName;
+  String speakerDesc;
+  String speakerImage;
+  String speakerInfo;
+  String speakerId;
+  String fbUrl;
+  String twitterUrl;
+  String linkedinUrl;
+  String githubUrl;
+  String speakerSession;
+  String sessionId;
+  String instagramUrl;
+  String behanceUrl;
+
+  Developer(
+      {this.speakerName,
+      this.speakerDesc,
+      this.speakerImage,
+      this.speakerInfo,
+      this.speakerId,
+      this.fbUrl,
+      this.twitterUrl,
+      this.linkedinUrl,
+      this.githubUrl,
+      this.speakerSession,
+      this.instagramUrl,
+      this.behanceUrl,
+      this.sessionId});
+}
